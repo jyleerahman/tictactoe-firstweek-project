b2aa9899c9f7ea061550d244b1089cdda7d57165
JY Lee <jylee.rahman@gmail.com>
2025-09-23 18:54:51 -0400

    Add initial file

--- tictactoe/src/App.tsx
+++ tictactoe/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <div className="grid grid-cols-3 gap-2">
           {gameState.board.map((b, i) => <button onClick={() => setGameState(makeMove(gameState, i))} className="w-full aspect-square" key={i}>{b}</button>)}
         </div>
-        <p>Winner is: {gameState.endState} </p>
+        <p className="font-bold"> 🎉 Winner is: {gameState.endState} 🎉 </p>
       </div>
     </>
   )
